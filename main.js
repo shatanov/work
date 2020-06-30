122759581758c6e0bd70f63f75a393545bc1b842
Timur Shatanov <timur-shatanov.work@mail.ru>
2020-06-30 15:08:39 +0300

    5 доработка

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let appData = {
         optionalExpenses: {
         },
         income: [],
-        savings: true
+        savings: true,
     };
 function chooseExpenses(){
     for(let i = 0; i < 2; i++){
@@ -31,22 +31,42 @@ function chooseExpenses(){
     };
 }
 chooseExpenses();
-
-appData.moneyPerDay = (appData.money/30).toFixed;
-
-alert("Ваш бюджет на день: " + appData.moneyPerDay);
-if(appData.money < 100){
-    console.log("мин");
-} else if(appData.money > 100 && appData.money < 2000){
-    console.log("ср");
-} else if(appData.money > 2000){
-    console.log("макс ");
-} else {
-    console.log("ошибка");
+function detectDayBudget(){
+    appData.moneyPerDay = (appData.money/30).toFixed();
+    alert("Ваш бюджет на день: " + appData.moneyPerDay);
 };
+detectDayBudget();
+function detectLevel(){
+    if(appData.money < 100){
+        console.log("мин");
+    } else if(appData.money > 100 && appData.money < 2000){
+        console.log("ср");
+    } else if(appData.money > 2000){
+        console.log("макс ");
+    } else {
+        console.log("ошибка");
+    };
+};
+detectLevel();
 function checkSavings(){
     if(appData.savings == true){
         let save = +prompt("Какова сумма накоплений?");
         let percent = +prompt("Какой процент накоплений?");
+        appData.mouthIncome = save/100/12*percent;
+        alert("Ваш доход по депозиту: " + appData.mouthIncome);
     }
 };
+checkSavings();
+let optionalExpensesNum = 1;
+function chooseOptExpenses(){
+ for(let i = 0; i < 3; i++ ){
+    let optionalExpensesAnswer = prompt("Статья необязательных расходов?");
+    if(optionalExpensesAnswer != "" && optionalExpensesAnswer != null) {
+        appData.optionalExpenses[optionalExpensesNum] = optionalExpensesAnswer;
+        optionalExpensesNum++;
+    }  else{
+        i = i - 1;
+    };
+ };
+};
+chooseOptExpenses();
